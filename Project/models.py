2745a770ba48437d4561812812d50b00881539de
Chris Chicas <christianchicas7@gmail.com>
2022-12-07 21:14:26 -0500

    added chest & treasure tables + created fkeys to connect tables

--- Project/models.py
+++ Project/models.py
@@ -12,6 +12,20 @@ class User(db.Model):
     username = db.Column(db.String(100), nullable = False, unique = True)
     password = db.Column(db.LargeBinary, nullable = False)
     role = db.Column(postgresql.ENUM("user", "admin", name="user_roles"), nullable = False, default = "user")
+    chests = db.relationship("Chest", backref = "user")
 
-# Add models for treasure chests (containers) and treasures (items)
-# Look into how to make relationships between all tables
+class Chest(db.Model):
+    __tablename__ = "chests"
+
+    chest_id = db.Column(db.Integer, primary_key = True)
+    chest_name = db.Column(db.String(100), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable = False)
+    treasures = db.relationship("Treasure", backref = "chest")
+
+class Treasure(db.Model):
+    __tablename__ = "treasures"
+
+    treasure_id = db.Column(db.Integer, primary_key = True)
+    treasure_title = db.Column(db.String(100), nullable = False)
+    treasure_details = db.Column(db.Text, nullable = False)
+    chest_id = db.Column(db.Integer, db.ForeignKey("chests.chest_id"), nullable = False)
