ea9fb2e4430b3d1bc2c67c4080f3f80bfbcb4389
Chris Chicas <christianchicas7@gmail.com>
2022-12-08 22:25:52 -0500

    added chest create, read, update, and delete routes to the dashboard

--- Project/dashboard.py
+++ Project/dashboard.py
@@ -1,4 +1,5 @@
 from flask import (Blueprint, render_template, redirect, request, session)
+from . import models
 
 bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
@@ -7,9 +8,78 @@ def dashboard():
     if not session.get("logged_in"):
         return redirect("/login")
 
-    return render_template("dashboard.html", user=session.get("user"))
+    chests = models.Chest.query.filter_by(user_id=session["user"]["user_id"]).all()
+    return render_template("dashboard.html", user=session.get("user"), chests=chests)
+
+@bp.route("/chests", methods=["POST"])
+def new_chest():
+    if not session.get("logged_in"):
+        return redirect("/login")
+
+    if request.method == "POST":
+        chest_name = request.form["chest_name"]
+
+        new_chest = models.Chest(chest_name=chest_name, user_id=session["user"]["user_id"])
+        models.db.session.add(new_chest)
+        models.db.session.commit()
+
+        return redirect("/dashboard")
+
+@bp.route("/chests/<string:name>", methods=["GET", "POST", "PUT", "DELETE"])
+def show_chest(name):
+    if not session.get("logged_in"):
+        return redirect("/login")
+
+    chest = models.Chest.query.filter_by(user_id=session["user"]["user_id"], chest_name=name).first()
+    treasures = models.Treasure.query.filter_by(chest_id=chest.chest_id).all()
+
+    if request.method == "POST": 
+        method = request.form["method"]
+
+        if method == "PUT":
+            chest_name = request.form["chest_name"]
+
+            chest.chest_name = chest_name
+            models.db.session.commit()
+
+            return redirect("/dashboard")
+        elif method == "DELETE":
+            models.db.session.delete(chest)
+            models.db.session.commit()
+
+            return redirect("/dashboard")  
+    # Post method made to accomodate html forms
+
+    if request.method == "PUT":
+        chest_name = request.form["chest_name"]
+
+        chest.chest_name = chest_name
+        models.db.session.commit()
+
+        return redirect("/dashboard")
+
+    if request.method == "DELETE":
+        models.db.session.delete(chest)
+        models.db.session.commit()
+
+        return redirect("/dashboard")
+
+    return render_template("chests/show.html", chest=chest, treasures=treasures)
+
+@bp.route("/chests/<string:name>/edit")
+def edit_chest(name):
+    if not session.get("logged_in"):
+        return redirect("/login")
+
+    chest = models.Chest.query.filter_by(user_id=session["user"]["user_id"], chest_name=name).first()
+
+    return render_template("chests/edit.html", chest=chest)
+
 
 @bp.route("/logout")
 def logout():
+    if not session.get("logged_in"):
+        return redirect("/login")
+
     session.clear()
     return redirect("/")
